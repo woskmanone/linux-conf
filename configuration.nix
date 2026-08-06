89fa6b61fe5468983388c6dd5630f7d9e7997c5d
Maksym Derypasko <maxumderypasko@gmail.com>
2026-08-06 21:13:36 +0300

    Refactor NixOS configuration with new services and settings
    Updated configuration for bootloader, networking, desktop environment, user accounts, and added new services.

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+ # Bootloader.
 boot.loader.systemd-boot.enable = false;
 boot.loader.efi.canTouchEfiVariables = true;
 
@@ -24,14 +24,10 @@ boot.loader.efi.canTouchEfiVariables = true;
     };
   };
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   networking.hostName = "nix-hplaptop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.wireless.enable = true;  
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -54,14 +50,113 @@ boot.loader.efi.canTouchEfiVariables = true;
     LC_TIME = "uk_UA.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
+  # Disable fucking X11 windowing system.
   services.xserver.enable = false;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."derypaskoms" = {
+    isNormalUser = true;
+    description = "Maksym Derypasko";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+  
+   # Install zsh.
+   programs.zsh = {
+   syntaxHighlighting.enable = true;
+     enable = true;
+   promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+   ohMyZsh = {
+       enable = true;
+       plugins = [ "git" "sudo" ];
+  };
+};
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+  #NN
+  qdirstat unzip wget 
+  #Networks
+  inetutils 
+  #Dev
+  vscodium git fastfetch btop gcc 
+  #Personal
+  librewolf telegram-desktop rustdesk 
+  ];
+
+  # Enable virtualisation
+   virtualisation.libvirtd.enable = true;
+   programs.virt-manager.enable = true;
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  services.tailscale = {
+    # Enable tailscale at startup
+    enable = true;
+  };
+
+  # Install WinBox
+   programs.winbox = {
+   enable = true;
+   openFirewall = true; 
+};
+  # Install Wireshark
+   programs.wireshark.enable = true;
+   programs.wireshark.package = pkgs.wireshark;
+
+  # Open ports in the firewall.
+   networking.firewall.allowedTCPPorts = [ ];
+   networking.firewall.allowedUDPPorts = [ ];
+   networking.firewall.trustedInterfaces = [ "tailscale0" ];
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
